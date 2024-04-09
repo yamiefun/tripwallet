4eadabd71800b415b75895107d2fd95a47037df3
yamiefun <yamiefun@gmail.com>
2024-04-09 10:14:56 +0800

    remove token

--- backend/tg/echobot.py
+++ backend/tg/echobot.py
@@ -13,8 +13,9 @@ bot.
 
 import logging
 
-from telegram import ForceReply, Update
+from telegram import ForceReply, Update, KeyboardButton, WebAppInfo, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import CallbackContext
 
 # Enable logging
 logging.basicConfig(
@@ -31,10 +32,15 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+    # await update.message.reply_html(
+    #     rf"Hi {user.mention_html()}!",
+    #     reply_markup=ForceReply(selective=True),
+    # )
+    kb = [
+        [KeyboardButton("Show me the web!",
+                        web_app=WebAppInfo("https://yamiefun.github.io/tripwallet/"))]
+    ]
+    await update.message.reply_text("Let's go", reply_markup=ReplyKeyboardMarkup(kb))
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -44,13 +50,14 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    prefix = "You said: "
+    await update.message.reply_text(prefix + update.message.text)
 
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("6901531325:AAEIes17OX-pncXaKeLkmu1W_kjWTR10ASQ").build()
+    application = Application.builder().token("").build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
@@ -58,6 +65,8 @@ def main() -> None:
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, launch_web))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
